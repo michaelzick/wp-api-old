8a0f6df98f3d117d4596f2215f90698a3907c6a7
Michael Zick <michaelzick@gmail.com>
2017-06-15 06:07:04 -0700

    INDEX: updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React        from 'react';
 import {render}     from 'react-dom';
 import App          from './components/App.js';
 import Home         from './components/Home.js';
-import DataActions  from './actions/DataActions.js';
 
 import {
   browserHistory,
@@ -12,13 +11,29 @@ import {
   Router
 } from 'react-router';
 
+import DataActions  from './actions/DataActions.js';
+
+
 class AppInitializer {
+  buildRoutes(data) {
+    return data.pages.map((page, i) => {
+      return (
+        <Route
+          component={ Home }
+          key={ page.id }
+          path={`/${page.slug}`}
+        />
+      );
+    });
+  }
+
   run() {
     DataActions.getPages((response)=>{
       render(
         <Router history={browserHistory}>
           <Route path="/" component={ App } >
             <IndexRoute component={ Home } />
+            {this.buildRoutes(response)}
           </Route>
           <Redirect from="*" to="/" />
         </Router>
